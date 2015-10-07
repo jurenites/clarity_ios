cd7e622cb0894921aaf2b59da03a409978d9124b
Alexey Klyotzin <aklyotzin@oysterlabs.com>
2015-10-07 18:18:04 +0300

    Begin avatar menu

---
+++ Clarity/UI/Orders/VCtrlAvatarMenu.swift
@@ -0,0 +1,17 @@
+//
+//  VCtrlAvatarMenu.swift
+//  Clarity
+//
+//  Created by Alexey Klyotzin on 07/10/15.
+//  Copyright © 2015 Spring. All rights reserved.
+//
+
+import UIKit
+
+class VCtrlAvatarMenu: UIViewController
+{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
